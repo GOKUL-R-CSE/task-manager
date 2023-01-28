d140121879594588437dbac146de8f74254c8cce
GOKUL_R_CSE <gokulr.cse2020@citchennai.net>
2023-01-28 17:30:26 +0530

    error handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require('../models/Task')
 const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
 const getAllTasks = asyncWrapper(async (req, res) => {
     const tasks = await Task.find({})
@@ -15,9 +16,7 @@ const getTask = asyncWrapper(async (req, res, next) => {
     const { id: taskID } = req.params
     const task = await Task.findOne({ _id: taskID })
     if (!task) {
-        const error = new Error('Not Found')
-        error.status = 404
-        return next(error)
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
     }
     res.status(200).json({ task })
 })
@@ -28,6 +27,9 @@ const updateTask = asyncWrapper(async (req, res) => {
         new: true, runValidators: true
 
     })
+    if (!task) {
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
+    }
     res.status(200).json({ task })
 
 })
@@ -36,12 +38,9 @@ const deleteTask = asyncWrapper(async (req, res) => {
     const task = await Task.findOneAndDelete({ _id: taskID })
 
     if (!task) {
-        return res.status(404).json({ msg: `No task with id : ${taskID}` })
+        return next(createCustomError(`No task with id : ${taskID}`, 404))
     }
 
-    if (!task) {
-        return res.status(404).json({ msg: `No task with id : ${taskID}` })
-    }
     res.status(200).json({ task })
 
 })
